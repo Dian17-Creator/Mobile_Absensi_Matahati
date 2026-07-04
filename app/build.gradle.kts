b97c51d98f9e0480ecb692fbc8dc5df34921064e
aditiyapratama <aditiyapratama2014@gmail.com>
2026-07-04 10:08:24 +0700

    Update : EdgeToEdge MainActiviy & Bottom Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "id.my.matahati.absensi"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.0.10"
+        versionCode = 11
+        versionName = "1.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
